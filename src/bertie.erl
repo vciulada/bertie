c5b810b14f07dfb9ca6c3e39d811047302e826a3
Vilius Čiulada <v.ciulada@gmail.com>
2016-10-14 12:45:15 +0300

    new project file

---
+++ src/bertie.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author CiulaVil
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 14. Spal. 2016 12:38
+%%%-------------------------------------------------------------------
+-module(bertie).
+-author("CiulaVil").
+
+%% API
+-export([start/0]).
+start() ->
+  io:format("Hello my name is Bertie~n").
